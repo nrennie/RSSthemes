7983c9cdd6d4de9af40c5a76be927e2938fcb8dd
nrennie <nrennie35@gmail.com>
2023-02-06 22:30:22 +0000

    update palettes

--- R/RSSPalettes.R
+++ R/RSSPalettes.R
@@ -4,7 +4,23 @@
 #'
 #' @export
 RSSPalettes <- list(
-  # sequential palettes
-  Significance = list(c("red", "lightgrey", "black"),
-                      "seq",
-                      FALSE))
+  signif_seq = list(c("#2F0907", "#5F120E", "#8E1B15",
+                      "#BE241C", "#EE2E24", "#F1574F",
+                      "#F4817B", "#F8ABA7", "#FBD5D3"),
+                    "seq",
+                    FALSE),
+  signif_div = list(c("#EE2E24", "#F1645D", "#F49A96",
+                      "#F7D1CF", "#FaFaFa", "#C8D4E1",
+                      "#85A1C1", "#426FA0", "#003D80"),
+                    "div",
+                    FALSE),
+  signif_qual = list(c("#EE2E24", "#F1645D", "#F49A96",
+                       "#F7D1CF", "#FaFaFa", "#C8D4E1",
+                       "#85A1C1", "#426FA0", "#003D80"),
+                     "div",
+                     FALSE)
+  )
+
+#' Significance red hex colour
+#' @export
+signif_red <- "#EE2E24"
