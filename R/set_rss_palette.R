fa3dabd313b0f14663f4a1a5a95cd8ce08777ad0
nrennie <nrennie35@gmail.com>
2023-05-25 16:54:17 +0100

    add base R palette function

---
+++ R/set_rss_palette.R
@@ -0,0 +1,15 @@
+#' Set RSS base R plotting palette
+#'
+#' @param palette Name of palette. See \code{names(RSSthemes::RSSPalettes)}.
+#' @export
+
+set_rss_palette <- function(palette, ...) {
+  
+  palette_choice <- RSSPalettes[[palette]]
+  
+  if (is.null(palette_choice) || is.numeric(palette)) {
+    stop("Palette does not exist. Use names(RSSPalettes) to find valid palette.")
+  }
+  
+  grDevices::palette(palette_choice[[1]])
+}
