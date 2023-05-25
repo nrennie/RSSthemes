b7fa6b3cc895487385ebb7133214883d4019b26d
nrennie <nrennie35@gmail.com>
2023-05-25 15:11:54 +0100

    edit theme

--- R/theme_significance.R
+++ R/theme_significance.R
@@ -2,77 +2,89 @@
 #'
 #' Custom ggplot theme
 #'
-#' @param main_font Font used for all text elements except the title. Default "sans".
-#' @param title_font Font used for the title. Default "sans".
+#' @param family Font used for all text elements. Default "Source Sans Pro".
+#' @param base_size Base font size for text elements. Default 12.
 #' @param hjust Horizontal alignment of title, subtitle, and caption. Default 0.
 #' @param text_col Text colour. Default "black".
 #' @param bg_col Background colour. Default "white".
 #' @return A ggplot2 theme
 #' @export
 
-theme_significance <- function(main_font = "sans",
-                               title_font = "sans",
+theme_significance <- function(family = "Source Sans Pro",
+                               base_size = 12,
                                hjust = 0,
                                text_col = "black",
                                bg_col = "white") {
-  ggplot2::theme_minimal() +
+  ggplot2::theme_minimal(
+    base_family = family,
+    base_size = base_size
+  ) +
     ggplot2::theme(
       # title
-      plot.title = ggplot2::element_text(family = title_font,
-                                face = "bold",
-                                size = 16,
-                                hjust = hjust,
-                                colour = text_col),
+      plot.title = ggplot2::element_text(
+        face = "bold",
+        hjust = hjust,
+        colour = text_col
+      ),
       plot.title.position = "plot",
       # subtitle
-      plot.subtitle = ggplot2::element_text(family = main_font,
-                                size = 14,
-                                hjust = hjust,
-                                colour = text_col),
+      plot.subtitle = ggplot2::element_text(
+        hjust = hjust,
+        colour = text_col
+      ),
       # caption
-      plot.caption = ggplot2::element_text(family = main_font,
-                                   size = 12,
-                                   hjust = hjust,
-                                   colour = text_col),
+      plot.caption = ggplot2::element_text(
+        hjust = hjust,
+        colour = text_col
+      ),
       # axis
-      axis.title = ggplot2::element_text(family = main_font,
-                                size = 12,
-                                colour = text_col),
-      axis.title.x = ggplot2::element_text(family = main_font,
-                                  size = 12,
-                                  margin = ggplot2::margin(t = 10, b = -10),
-                                  colour = text_col),
-      axis.title.y = ggplot2::element_text(family = main_font,
-                                  size = 12,
-                                  margin = ggplot2::margin(r = 10, l = -10),
-                                  colour = text_col),
-      axis.text = ggplot2::element_text(family = main_font,
-                               size = 10,
-                               colour = text_col),
+      axis.title = ggplot2::element_text(
+        colour = text_col
+      ),
+      axis.title.x = ggplot2::element_text(
+        margin = ggplot2::margin(t = 10, b = -10),
+        colour = text_col
+      ),
+      axis.title.y = ggplot2::element_text(
+        margin = ggplot2::margin(r = 10),
+        colour = text_col
+      ),
+      axis.text = ggplot2::element_text(
+        colour = text_col
+      ),
       # plot
-      plot.margin = ggplot2::unit(c(0.8, 0.8, 0.5, 0.5),
-                         unit = "cm"),
-      plot.background = ggplot2::element_rect(fill = bg_col,
-                                              colour = bg_col),
-      panel.background = ggplot2::element_rect(fill = bg_col,
-                                               colour = bg_col),
+      plot.margin = ggplot2::margin(10, 10, 10, 10),
+      plot.background = ggplot2::element_rect(
+        fill = bg_col,
+        colour = bg_col
+      ),
+      panel.background = ggplot2::element_rect(
+        fill = bg_col,
+        colour = bg_col
+      ),
       # legend
       legend.position = "bottom",
-      legend.text = ggplot2::element_text(family = main_font,
-                                 size = 10,
-                                 colour = text_col),
-      legend.title = ggplot2::element_text(family = main_font,
-                                  size = 12,
-                                  colour = text_col),
-      legend.background = ggplot2::element_rect(fill = "transparent",
-                                       colour = "transparent"),
-      legend.key = ggplot2::element_rect(fill = "transparent",
-                                colour = "transparent"),
+      legend.text = ggplot2::element_text(
+        colour = text_col
+      ),
+      legend.title = ggplot2::element_text(
+        colour = text_col
+      ),
+      legend.background = ggplot2::element_rect(
+        fill = "transparent",
+        colour = "transparent"
+      ),
+      legend.key = ggplot2::element_rect(
+        fill = "transparent",
+        colour = "transparent"
+      ),
       # facets
-      strip.background = ggplot2::element_rect(fill = "transparent",
-                                      colour = "transparent"),
-      strip.text = ggplot2::element_text(family = main_font,
-                                size = 14,
-                                colour = text_col)
+      strip.background = ggplot2::element_rect(
+        fill = "transparent",
+        colour = "transparent"
+      ),
+      strip.text = ggplot2::element_text(
+        colour = text_col
+      )
     )
 }
