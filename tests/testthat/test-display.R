75e13a6242fa7b4d1a6f1c1684ee49a8f76ca1f2
nrennie <nrennie35@gmail.com>
2024-02-27 23:38:50 +0000

    add unit tests

--- tests/testthat/test-display.R
+++ tests/testthat/test-display.R
@@ -4,9 +4,9 @@ test_that("palette display", {
   expect_error(view_all_palettes(type = "seq"))
 
   expect_error(RSScols("new_name"))
-  expect_error(RSScols("signif_qual", n = 4))
   expect_error(RSScols("signif_qual", type = "cont"))
   expect_error(RSScols("signif_qual", direction = 0))
+  expect_silent(RSScols("signif_qual", n = 4))
   expect_silent(RSScols("signif_qual", direction = -1))
 
 })
